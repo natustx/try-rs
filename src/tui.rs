9b1fdc2e54cd2cb5030981b2a87a8dec381871c1
Tassio Virginio <tassiovirginio@gmail.com>
2026-02-24 19:16:53 -0300

    chore: revert to v1.5.0 and bump version to v1.5.2

--- src/tui.rs
+++ src/tui.rs
@@ -1,8 +1,8 @@
 use anyhow::Result;
 use chrono::Local;
 use crossterm::event::{self, Event, KeyCode};
-use fuzzy_matcher::skim::SkimMatcherV2;
 use fuzzy_matcher::FuzzyMatcher;
+use fuzzy_matcher::skim::SkimMatcherV2;
 use ratatui::{prelude::*, widgets::*};
 
 use std::{
